f764cfa5fab8098d25731fc6705f6b50675a19a0
TheCanadainBrownie <shahar.amin7@gmail.com>
2014-07-30 15:25:55 -0700

    Need Help

---
+++ app/src/main/java/com/prototype/ryersonapp/ExploreView.java
@@ -0,0 +1,44 @@
+package com.prototype.ryersonapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Shahar on 2014-07-29.
+ */
+public class ExploreView extends Activity {
+
+
+    ExploreViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.explore_layout);
+        Intent i =getIntent();
+        setTitle(i.getStringExtra("Name"));
+
+
+        adapter = new ExploreViewAdapter(this, generateData());
+
+
+        ListView listView = (ListView) findViewById(R.id.ListView1);
+
+
+        listView.setAdapter(adapter);
+
+
+    }
+
+
+    private ArrayList<Items> generateData(){
+        ArrayList<Items> items = new ArrayList<Items>();
+        items.add(new Items("Item 1"));
+        return items;
+    }
+}
---
+++ app/src/main/java/com/prototype/ryersonapp/Items.java
@@ -0,0 +1,22 @@
+package com.prototype.ryersonapp;
+
+/**
+ * Created by Shahar on 2014-07-29.
+ */
+public class Items {
+
+    private String name;
+
+    public Items(String name) {
+        super();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
